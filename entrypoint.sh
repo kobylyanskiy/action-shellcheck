2c35c7fdd66a7befde597e371d4cf5d3edb8bb3f
Kirill Kobylyanskiy <k.kobylyanskiy@gmail.com>
2020-03-26 13:13:27 +0300

    Write shellcheck output to stdout

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
   # erroformat: https://git.io/JeGMU
   shellcheck -f json  ${INPUT_SHELLCHECK_FLAGS:-'--external-sources'} $(find "${INPUT_PATH:-'.'}" -path "./build/*" -prune -o -path "./meta-layers/*" -prune -o -type f -name "${INPUT_PATTERN:-'*.sh'}" -print) \
-    | jq -r '.[] | "\(.file):\(.line):\(.column):\(.level):\(.message) [SC\(.code)](https://github.com/koalaman/shellcheck/wiki/SC\(.code))"' \
+    | tee /dev/tty | jq -r '.[] | "\(.file):\(.line):\(.column):\(.level):\(.message) [SC\(.code)](https://github.com/koalaman/shellcheck/wiki/SC\(.code))"' \
     | reviewdog -efm="%f:%l:%c:%t%*[^:]:%m" -name="shellcheck" -reporter=github-pr-review -level="${INPUT_LEVEL}"
 else
   # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
