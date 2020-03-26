7964199a2f7b34745eaf307057d1df34f5175bce
Kirill Kobylyanskiy <k.kobylyanskiy@gmail.com>
2020-03-26 13:22:37 +0300

    Update error code

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,8 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 if [ "${INPUT_REPORTER}" = 'github-pr-review' ]; then
   # erroformat: https://git.io/JeGMU
   shellcheck -f json  ${INPUT_SHELLCHECK_FLAGS:-'--external-sources'} $(find "${INPUT_PATH:-'.'}" -path "./build/*" -prune -o -path "./meta-layers/*" -prune -o -type f -name "${INPUT_PATTERN:-'*.sh'}" -print) \
-    | tee /dev/tty | jq -r '.[] | "\(.file):\(.line):\(.column):\(.level):\(.message) [SC\(.code)](https://github.com/koalaman/shellcheck/wiki/SC\(.code))"' \
-    | reviewdog -efm="%f:%l:%c:%t%*[^:]:%m" -name="shellcheck" -reporter=github-pr-review -level="${INPUT_LEVEL}"
+    | tee output | jq -r '.[] | "\(.file):\(.line):\(.column):\(.level):\(.message) [SC\(.code)](https://github.com/koalaman/shellcheck/wiki/SC\(.code))"' \
+    | reviewdog -efm="%f:%l:%c:%t%*[^:]:%m" -name="shellcheck" -reporter=github-pr-review -level="${INPUT_LEVEL}"; ERR_CODE=$?; cat output; rm output; exit $ERR_CODE
 else
   # github-pr-check,github-check (GitHub Check API) doesn't support markdown annotation.
   shellcheck -f checkstyle ${INPUT_SHELLCHECK_FLAGS:-'--external-sources'} $(find "${INPUT_PATH:-'.'}" -not -path "${INPUT_EXCLUDE}" -type f -name "${INPUT_PATTERN:-'*.sh'}") \
